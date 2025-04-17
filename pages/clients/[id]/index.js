382bf82896dc68167e58251afc1c617cf01251a4
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-17 15:59:04 +0530

    :zap: navigating programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,5 +1,19 @@
+import { useRouter } from "next/router"
+
 export default function ClientIdPage() {
+
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    // load data...
+    // router.push('/clients/max/projecta')
+    router.push({
+      pathname: '/clients/[id]/[clientProjectId]',
+      query: { id: 'max', clientProjectId: 'projecta' }
+    })
+  }
   return <div>
     <h1>Client Id page</h1>
+    <button onClick={loadProjectHandler}>Load project A</button>
   </div>
 }
