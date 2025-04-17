6b956afae7e9ad69d1e990459d6ee7eac21708ee
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-17 15:32:32 +0530

    :zap: :art: link component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return <div>
     <h1>Home Page</h1>
+    <ul>
+      <li>
+        <Link href="/portfolio">Portfolio</Link>
+      </li>
+    </ul>
   </div>
 }
